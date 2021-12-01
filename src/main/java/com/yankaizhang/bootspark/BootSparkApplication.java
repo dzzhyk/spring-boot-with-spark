c66cd6c6556ccf7917c0f99403f07a759ba1b187
dzzhyk <dzzhyk@qq.com>
2021-12-02 00:08:17 +0800

    initial commits

---
+++ src/main/java/com/yankaizhang/bootspark/BootSparkApplication.java
@@ -0,0 +1,14 @@
+package com.yankaizhang.bootspark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSparkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootSparkApplication.class, args);
+        int a = 100;
+    }
+
+}
---
+++ src/main/scala/com/yankaizhang/bootspark/WordCount.scala
@@ -0,0 +1,26 @@
+package com.yankaizhang.bootspark
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+import org.slf4j.LoggerFactory
+
+object WordCount{
+
+  private val log = LoggerFactory.getLogger(WordCount.getClass)
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("wordcount").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+
+    // 读取数据
+    val fileLines: RDD[String] = sc.textFile("data/input/words.txt")
+
+    // 数据操作/转换
+    val words: RDD[String] = fileLines.flatMap(fileLines => {
+      fileLines.split(" ")
+    })
+
+    println(words.toString())
+
+  }
+}
